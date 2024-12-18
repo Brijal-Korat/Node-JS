9e80b0410ffec8ba04ca578d761711670dada439
Brijal-Korat <brijalkorat@gmail.com>
2024-12-18 10:19:14 +0530

    Node-js

---
+++ Lecture_3.Modules/index.js
@@ -0,0 +1,7 @@
+const { sum, sub, mul } = require("./calculationModules");
+
+console.log("Modules in Node-JS");
+
+console.log(`Addition :- ${sum(10,5)}`);
+console.log(`Substraction :- ${sub(50,15)}`);
+console.log(`Multiplication :- ${mul(10,5)}`);
