942953d0b16f5026204a223fc7cab7a8eb421535
Brijal-Korat <brijalkorat@gmail.com>
2024-12-23 10:28:02 +0530

    Node-js

--- Lecture_4.Create-server/index.js
+++ Lecture_4.Create-server/index.js
@@ -3,7 +3,6 @@ const http = require(`http`);
 const port = 4000;
 
 const server = http.createServer((req,res) => {
-    res.write(`<h1>hello world..!</h1>`);
     res.write(`<h1>hello node.js..!</h1>`);
     res.write(`<p>The paragraph of node.js..!</p>`);
     res.end();
