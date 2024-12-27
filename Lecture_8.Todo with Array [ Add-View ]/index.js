6204fc022ea3e8af68a6aa84753c863ddb911328
Brijal-Korat <brijalkorat@gmail.com>
2024-12-27 10:36:27 +0530

    Node-js

---
+++ Lecture_8.Todo with Array [ Add-View ]/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const port = 9000;
+const app = express();
+
+app.set('view engine','ejs');
+app.use(express.urlencoded());
+let record = [];
+
+app.get('/',(req,res) => {
+    return res.render('table',{
+        record
+    });
+})
+
+app.get('/add',(req,res) => {
+    return res.render('form');
+})
+
+app.post('/adduser',(req,res) => {
+    const {username, userphone} = req.body;
+    let obj = {
+        name : username,
+        phone : userphone
+    }
+    record.push(obj);
+    console.log("Successfully added..!");
+    return res.redirect('/');
+})
+
+app.listen(port,(err) => {
+    if(err){
+        console.log(err);
+        return false;
+    }
+    console.log(`Server is running on port :- ${port}`);
+})
---
+++ Lecture_9.Todo with Array [ Add-View-Delete ]/index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const port = 9000;
+const app = express();
+
+app.set('view engine','ejs');
+app.use(express.urlencoded());
+let record = [];
+
+app.get('/',(req,res) => {
+    return res.render('table',{
+        record
+    });
+})
+
+app.get('/add',(req,res) => {
+    return res.render('form');
+})
+
+app.post('/adduser',(req,res) => {
+    const {username, userphone} = req.body;
+    let obj = {
+        id: Math.floor(Math.random()*10000),
+        name : username,
+        phone : userphone
+    }
+    record.push(obj);
+    console.log("Successfully added..!");
+    return res.redirect('/');
+})
+
+app.get('/deleteUser',(req,res) => {
+    let id = req.query.delId;
+    let deleteData = record.filter(val => val.id != id);
+    record = deleteData;
+    return res.redirect('/');
+})
+
+app.listen(port,(err) => {
+    if(err){
+        console.log(err);
+        return false;
+    }
+    console.log(`Server is running on port :- ${port}`);
+})
