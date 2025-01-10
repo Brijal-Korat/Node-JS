cdf1dcc18b8dacf6891a139b416db53a4b5ca25d
Brijal-Korat <brijalkorat@gmail.com>
2025-01-10 20:31:55 +0530

    Node-js

--- Lecture_12.Convert HTML to EJS/index.js
+++ Lecture_12.Convert HTML to EJS/index.js
@@ -10,6 +10,28 @@ app.get('/',(req,res) => {
     return res.render('dashboard');
 })
 
+app.get('/charts',(req,res) => {
+    return res.render('charts');
+})
+app.get('/widgets',(req,res) => {
+    return res.render('widgets');
+})
+app.get('/tables',(req,res) => {
+    return res.render('tables');
+})
+app.get('/grid',(req,res) => {
+    return res.render('grid');
+})
+app.get('/form-basic',(req,res) => {
+    return res.render('form-basic');
+})
+app.get('/form-wizard',(req,res) => {
+    return res.render('form-wizard');
+})
+app.get('/pages-buttons',(req,res) => {
+    return res.render('pages-button');
+})
+
 app.listen(port,(err) => {
     if(err) {
         console.log(err);
