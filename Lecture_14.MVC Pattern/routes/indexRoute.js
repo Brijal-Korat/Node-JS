d545e433a7d417623a99436d968401aac91a3a3d
Brijal-Korat <brijalkorat@gmail.com>
2025-01-27 10:48:56 +0530

    Node-js

--- Lecture_14.MVC Pattern/routes/indexRoute.js
+++ Lecture_14.MVC Pattern/routes/indexRoute.js
@@ -3,6 +3,8 @@ const express = require('express');
 const routes = express.Router();
 
 routes.use('/crud',require('./crudRoute'))
-// routes.use('/product',require('./productRoute'))
+routes.use('/product',require('./productRoute'))
+routes.use('/blog',require('./blogRoute'))
+routes.use('/contact',require('./contactRoute'))
 
 module.exports = routes;
